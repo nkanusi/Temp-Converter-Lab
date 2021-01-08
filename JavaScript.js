4ded9b0a68e77b65d35e802c34057593bdb6a387
Amarachukwu Ugoanusi <nkanusi@gmail.com>
2021-01-08 11:43:50 -0500

    part 2

--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,25 @@
 const button = document.getElementById("buttonId");
-const finalResult = document.getElementById("result");
-
-
+const fahrenheitResult = document.getElementById("Fresult");
+const celciusResult = document.getElementById("Cresult");
+const kelvinResult = document.getElementById("Kresult");
+const rankineResult = document.getElementById("Rresult");
+let scale = document.getElementById("scale");
 
 
 button.addEventListener('click',  () => {
     const inputValue = document.getElementById('value').value;
-    let result = (inputValue - 32) * 5 / 9 ;
-    return finalResult.innerHTML = result ;
     
+    let fresult = inputValue, cresult = (inputValue - 32) * 5 / 9 ,
+     kresult = cresult + 273.15, rresult= kresult * 1.8;
+     
 
-});
+    // console.log(rresult);
+    // console.log(fresult);
+
+    return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+
+});//calculate button
+
+
+
+
