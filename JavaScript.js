ce53fa4cb5b844b50e7af2bfa6b8e27428f60446
Amarachukwu Ugoanusi <nkanusi@gmail.com>
2021-01-09 17:11:26 -0500

    updated for scale option

--- JavaScript.js
+++ JavaScript.js
@@ -3,23 +3,61 @@ const fahrenheitResult = document.getElementById("Fresult");
 const celciusResult = document.getElementById("Cresult");
 const kelvinResult = document.getElementById("Kresult");
 const rankineResult = document.getElementById("Rresult");
-let scale = document.getElementById("scale");
 
 
-button.addEventListener('click',  () => {
-    const inputValue = document.getElementById('value').value;
-    
-    let fresult = inputValue, cresult = (inputValue - 32) * 5 / 9 ,
-     kresult = cresult + 273.15, rresult= kresult * 1.8;
-     
+function calculateTemperature(){
+   let scale = document.getElementById("scale").value;
+   const inputValue = document.getElementById('value').value;
+   switch(scale) {
+      case "fahrenheit":
+        convertFromFahrenheit(inputValue);
+        break;
+      case "celcius":
+        convertFromCelcius(inputValue);
+        break;
 
-    // console.log(rresult);
-    // console.log(fresult);
+      case "kelvin":
+        convertFromKelvin(inputValue);
+        break;
+      case "rankine":
+        convertFromRankine(inputValue);
+        break;
+  }
+}
 
-    return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+function convertFromFahrenheit(inputValue){
+  
+  let fresult = inputValue,
+  cresult = Math.round((inputValue - 32) * 5 / 9 ),
+  kresult = Math.round(cresult + 273.15),
+  rresult= Math.round(kresult * 1.8);
+  return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+}
 
-});//calculate button
+function convertFromCelcius(inputValue){
+  
+  let cresult = inputValue,
+  fresult = cresult * 1.8 + 32,
+  kresult = cresult + 273.15,
+  rresult = (cresult + 273.15 ) * 1.8;
 
+  return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+}
 
+function convertFromKelvin(inputValue){
+  
+  let kresult = inputValue,
+  cresult = Math.round(kresult - 273.15),
+  fresult = Math.round(kresult * 1.8 - 459.67),
+  rresult= kresult * 1.8;
+  return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+}
 
-
+function convertFromRankine(inputValue){
+  
+  let rresult = inputValue,
+  cresult = (inputValue - 491.67)/1.8;
+  fresult = rresult - 459.67;
+  kresult= rresult * 5/9 ; 
+  return celciusResult.textContent = cresult , kelvinResult.textContent = kresult, fahrenheitResult.textContent = fresult, rankineResult.textContent = rresult;
+}
